9ec3c82ba8c813ba3d7d3001bf9927d31c0751bc
Gokul <gokul@ubuntu-22.04>
2023-03-30 10:15:05 +0530

    add_files

---
+++ 2task_1.js
@@ -0,0 +1,9 @@
+function greatNum(num1,num2){
+    if(num1>num2){
+        return "The greater number of "+num1+" and "+num2+" is "+num1;
+    }
+    else{
+        return "The greater number of "+num2+" and "+num1+" is "+num2;
+    }
+}
+console.log(greatNum(5,10));
